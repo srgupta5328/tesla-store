dbc0f23cc9a1294310879dedfbd24f627feea025
Rohan Gupta <srgupta5328@gmail.com>
2018-12-21 16:31:44 -0500

    initializing the boiler plate postgres code

--- main.go
+++ main.go
@@ -16,6 +16,26 @@ const (
 
 var storeDB *sql.DB
 
+//InitDB initializes the database
+func InitDB() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	fmt.Println(psqlInfo)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully Connected!")
+}
+
 func main() {
 	fmt.Println("Initializing project")
 }
