23ce73a3f02434f598d6e214969abceed0f1c1b2
Rohan Gupta <srgupta5328@gmail.com>
2018-12-21 14:54:21 -0500

    defining the interface and the generator functions for the uuid

--- main.go
+++ main.go
@@ -1,12 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
+	"github.com/satori/go.uuid"
+)
+
+//Tesla Struct holds the data for a tesla vehicle
 type Tesla struct {
-	ID    string `json:"ID"`
-	Make  string `json:"Make"`
-	Model string `json:"Model"`
-	Cost  int64  `json:"Cost"`
+	ID       string `json:"ID,omitempty"`
+	Make     string `json:"Make,omitempty"`
+	Model    string `json:"Model,omitempty"`
+	ImageURL string `json:"image_url,omitempty"`
+	Owned    bool   `json:"owned,omitempty"`
+	Cost     int64  `json:"Cost,omitempty"`
+}
+
+/*
+TeslaDB interface is used to make sure all the interactions with the database are
+thread safe
+*/
+type TeslaDB interface {
+	//ListVehicles lists all the Teslas in the Vehicles table
+	ListVehicles() ([]*Tesla, error)
+
+	//AddTesla adds a Tesla to the database and returns a tesla object and an error
+	AddTesla() (*Tesla, error)
+
+	//UpdateTesla edits info of an existing Tesla and returns an object and an error
+	UpdateTesla() (*Tesla, error)
+}
+
+/*
+Utility function to set the tesla ID for a given tesla vehicle, this is the unique identifier for
+each object
+*/
+
+func (t *Tesla) setTeslaID() string {
+	u1, err := uuid.NewV1()
+	if err != nil {
+		fmt.Printf("Error Setting the TeslaID: %s", err.Error())
+	}
+	t.ID = u1.String()
+
+	return t.ID
 }
 
 func main() {
